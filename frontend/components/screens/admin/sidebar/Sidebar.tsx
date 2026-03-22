a2bdf01eb0ef269d498e321e5ecff2e161bf795d
Reddripill <reddripill@mail.ru>
2026-03-22 23:12:00 +0300

    fix: delete props pass in sidebar

--- frontend/components/screens/admin/sidebar/Sidebar.tsx
+++ frontend/components/screens/admin/sidebar/Sidebar.tsx
@@ -99,7 +99,6 @@ const Sidebar = ({ session }: ISession) => {
          <MobileHeader
             isOpen={isOpen}
             setIsOpen={setIsOpen}
-            username={session.name}
             logout={async () => {
                await logoutHandler();
             }}
