641f1078628ae8d4353e7282610ac1b8947b1366
Reddripill <reddripill@mail.ru>
2026-08-08 13:51:00 +0300

    divide tests in separate files

--- frontend/components/UI/tournament-card/admin-card/AdminCardItem.test.tsx
+++ frontend/components/UI/tournament-card/admin-card/AdminCardItem.test.tsx
@@ -6,51 +6,10 @@ import { render } from "../../../../tests/test-utils";
 import { server } from "../../../../tests/mocks/server";
 import { http, HttpResponse } from "msw";
 import { API } from "../../../../constants/api";
-import { IBaseEntityWithTitleAndCount } from "../../../../types/main.types";
-import { ITournament } from "../../../../types/entities.types";
+
 import userEvent from "@testing-library/user-event";
 import tournaments from "../../../../tests/mocks/data/tournaments.data.json";
 import { IUpdateCompetitionStatusBody } from "../../../../types/query.types";
-
-test("Check component behavior while tournaments data is exist", async () => {
-   render(<HomePage session={SESSIONS.ADMIN} />);
-   expect(
-      screen.getByLabelText("Loading", { selector: "svg" }),
-   ).toBeInTheDocument();
-
-   expect(await screen.findByText("Список соревнований")).toBeInTheDocument();
-});
-
-test("Check component behavior while tournaments data is not exist", async () => {
-   server.use(
-      http.get(API.TOURNAMENTS, () => {
-         return HttpResponse.json<IBaseEntityWithTitleAndCount<ITournament>>({
-            count: 0,
-            data: [],
-         });
-      }),
-   );
-
-   render(<HomePage session={SESSIONS.ADMIN} />);
-
-   expect(await screen.findByText("Записей не найдено")).toBeInTheDocument();
-});
-
-test("Check component behavior while error", async () => {
-   server.use(
-      http.get(API.TOURNAMENTS, () => {
-         return HttpResponse.json(null, {
-            status: 500,
-         });
-      }),
-   );
-
-   render(<HomePage session={SESSIONS.ADMIN} />);
-
-   expect(
-      await screen.findByText("Ошибка получения данных"),
-   ).toBeInTheDocument();
-});
 
 const checkClickStatusBtn = (tournament_id: string, id: string) => {
    const requestState: {
